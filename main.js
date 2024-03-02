88254fc180c933c24af47b03b788453e87d263c7
AstralTurtle (Aaron James) <jamesacs329@gmail.com>
2024-03-02 10:32:18 -0500

    me when i dont push

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
-const { BrowserWindow } = require("electron")
+const {app, BrowserWindow, screen } = require("electron")
 
-const widget = new BrowserWindow({
-    frame: false,
-    transparent:true
+const createWindow = () => {
+    const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+    console.log(width,height);
+    // Specify the size and position of the window
+    const windowWidth = 600;
+    const windowHeight = 600;
+    const x = width - windowWidth; // position from the right
+    const y = height - windowHeight; // position from the bottom
+
+    const win = new BrowserWindow({
+        width: windowHeight,
+        height: windowHeight,
+        // transparent: true,
+        frame: false,
+        resizable:false,
+        x: width,
+        y: height,
+    });
+    win.setAlwaysOnTop(true);
+    win.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+    createWindow();
 });
-
